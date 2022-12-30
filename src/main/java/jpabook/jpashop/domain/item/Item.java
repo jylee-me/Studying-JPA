72b44e361b0b0ebf445a536630666f23575c3d56
jylee-me <108018021+jylee-me@users.noreply.github.com>
2022-12-31 02:17:39 +0900

    Feat/1 product domain development (#3)
    * 상품 엔티티 개발(비즈니스 로직 추가)
    
    * #001 feat: 상품 리포지토리 개발
    
    * #001 feat: 상품 서비스 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,5 +26,31 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /* 비즈니스 로직 */
+    /* 도메인 주도 설계를 할 때 엔티티 자체에서 해결할 수 있는 것들은 엔티티 안에 비즈니스 로직을 넣는게 좋음 -> 객체지향적임
+    * stockQuantity라는 재고수량이 Item 엔티티 안에 있음
+    * 그러면 이 stockQuantity 데이터를 가지고 있는 데서 Item에서 비즈니스 로직을 처리하는 게 좋음 */
+
+    /*
+    * stock 증가
+    */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+        // 재고가 0보다 적으면 안됨.
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }
 
