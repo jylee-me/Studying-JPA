0cb553f261e22209280237160f56a88537a3af76
jylee-me <jylee9432@gmail.com>
2023-01-01 16:16:21 +0900

    주문, 주문 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -32,5 +32,24 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);                // 주문 상품
+        orderItem.setOrderPrice(orderPrice);    // 가격
+        orderItem.setCount(count);              // 수량
 
+        item.removeStock(count);                // 주문된 수량만큼 재고에서 차감
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        // 취소하면 아이템의 재고를 원복해야 함.
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
