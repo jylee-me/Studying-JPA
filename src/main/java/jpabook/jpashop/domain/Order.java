af6bcbf9656db947ad5a093c81dffb6da7bc77d1
jylee-me <108018021+jylee-me@users.noreply.github.com>
2023-01-05 16:57:51 +0900

    Feat/2 order domain development (#5)
    * 주문, 주문 상품 엔티티 개발
    
    * #002 feat: 주문, 주문 상품 엔티티 개발
    
    * #002 feat: 주문 리포지토리 개발
    
    * #002 feat: 주문 서비스 개발
    
    * commit
    
    * #002 feat: 주문 기능 테스트
    
    * #002 feat: 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -57,4 +57,77 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+
+    /* 주문 생성하는 것은 복잡함. Order만 생성하는 것이 아니라, OrderItem도 있어야 하고 Delivery도 있어야 하고 여러 연관 관계가 얽혀있음.
+    * 이런 복잡한 생성은 별도의 생성 메서드가 있으면 좋다.
+    * */
+
+    /* 가변인자
+    * OrderItem... orderItems에서 '...'을 가변인자라고 한다.
+    * */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);         // 처음 상태를 ORDER로 강제해놓음
+        order.setOrderDate(LocalDateTime.now());    // 주문 상태는 현재 시각으로 설정
+        return order;
+    }
+    
+    /* 이렇게 생성 메서드를 작성하는 게 중요한 이유:
+    * 앞으로 뭔가 생성하는 시점을 변경해야 하면 이 메서드만 바꾸면 됨. <<- 중요 포인트!
+    *   -->> 근데 뭔 말임??
+    * */
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();         // 한 사람이 여러개의 아이템을 주문할 수 있고 그 아이템 각각 취소룰 해주는 메서드가 필요함.
+        }
+    }
+
+
+    //==조회 로직==//
+    /*
+     * 주문 상품 전체 가격 조회
+     * */
+/*    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }*/
+
+    public int getTotalPrice() {
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -32,5 +32,23 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);                // 주문 상품
+        orderItem.setOrderPrice(orderPrice);    // 가격
+        orderItem.setCount(count);              // 수량
 
+        item.removeStock(count);                // 주문된 수량만큼 재고에서 차감
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);      // 주문 취소 시 아이템의 재고를 원복
+    }
+
+    public int getTotalPrice() {        // 주문 상품의 전체 가격 조회
+        return getOrderPrice() * getCount();
+    }
 }
